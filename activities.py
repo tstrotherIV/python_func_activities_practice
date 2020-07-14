fa205784aaa3f5661086afecb4de0e9b2ff99eb1
Tom Strother <tom_strother@yahoo.com>
2020-07-14 14:46:12 -0500

    initial commit

---
+++ activities.py
@@ -0,0 +1,34 @@
+# Running Kids Function
+def run(*kids):
+    for kid in kids:
+        print(f"{kid} ran like a fool!")
+
+
+run("Pam", "Same", "Andrea", "Will")
+
+
+# Swinging Kids Function
+def swing(*kids):
+    for kid in kids:
+        print(f"{kid} likes to swing!")
+
+
+swing("Marybeth", "Ariel", "Kevin", "Courtney")
+
+
+# Sliding Kids Function
+def sliding(*kids):
+    for kid in kids:
+        print(f"{kid} likes to slide to much!")
+
+
+sliding("Mike", "Jack", "Jennifer", "Earl")
+
+
+# Hide and Seek Function
+def hideandseek(*kids):
+    for kid in kids:
+        print(f"{kid} is playing Hide and Seek")
+
+
+hideandseek("Henry", "Heather", "Hayley", "Hugh")
